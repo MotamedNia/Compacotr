109ee4ddf15c7015de8159d37913fb1426cdaa55
Soheil-M <hossein.mo.re@gmail.com>
2015-02-28 10:23:41 +0330

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.lang.String;
+public class Main
+{
+	static public void main(String[] args)
+	{
+		System.out.print("Hello World");
+		System.out.println("it Finally Worked");
+	}
+ 
+	
+}
